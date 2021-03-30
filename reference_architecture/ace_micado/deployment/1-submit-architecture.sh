879ce65fa984fef985c6db4ea57cfc0855d9d139
Dimitris Kagialis <kagialisd@gmail.com>
2021-03-30 21:54:56 +0300

    Docker image + ADT

--- reference_architecture/ace_micado/deployment/1-submit-architecture.sh
+++ reference_architecture/ace_micado/deployment/1-submit-architecture.sh
@@ -24,5 +24,5 @@ if [ -z "$SSL_PASS" ]; then
   exit
 fi
 
-echo "Submitting pithia.yml to MiCADO at $MICADO_MASTER with appid \"$APP_ID\"..."
+echo "Submitting ace.yml to MiCADO at $MICADO_MASTER with appid \"$APP_ID\"..."
 curl --insecure -s -F adt=@"ace.yml" -X POST -u "$SSL_USER":"$SSL_PASS" https://$MICADO_MASTER:$MICADO_PORT/toscasubmitter/v2.0/applications/$APP_ID/ | jq
